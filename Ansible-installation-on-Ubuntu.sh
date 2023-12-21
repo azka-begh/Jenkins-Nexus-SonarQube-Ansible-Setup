e3a558b42fcf4606303c16d917f05ba30cf25ff6
azka-begh <97597065+azka-begh@users.noreply.github.com>
2023-12-21 18:29:34 +0530

    Update Ansible-installation-on-Ubuntu.sh

--- Ansible-installation-on-Ubuntu.sh
+++ Ansible-installation-on-Ubuntu.sh
@@ -6,3 +6,7 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 mkdir -p ~/ansible/
+
+#When ansible is installed, then go to /etc/ansible/ and take a backup of ansible.cfg 
+#and run ansible-config init --disabled -t all > ansible.cfg
+#or simple create an ansible.config file in your working ansible directory where you disable host_key_checking
