e2debe9dbbb6e5f991a5aa1c086f8ccfece0e771
azka-begh <97597065+azka-begh@users.noreply.github.com>
2024-01-28 16:36:51 +0530

    Update and rename Jenkins-with-Docker to Docker/Jenkins-with-Docker.sh

--- Docker/Jenkins-with-Docker.sh
+++ Docker/Jenkins-with-Docker.sh
@@ -1,23 +1,21 @@
+#!/bin/bash
 docker volume create --name jenkins-data
------------------------------------------------------
-sudo vim /etc/systemd/system/jenkins.service
-
+sudo cat <<EOT>> /etc/systemd/system/jenkins.service
 [Unit]
 Description=jenkins service
 After=network.target
-
 [Service]
-
 SyslogIdentifier=jenkins.service
 ExecStartPre=-/usr/bin/docker create --name jenkins -p 8080:8080 -v jenkins-data:/var/jenkins_home --restart=always jenkins/jenkins
 ExecStart=/usr/bin/docker start -a jenkins
 ExecStop=-/usr/bin/docker stop --time=0 jenkins
-
 [Install]
 WantedBy=multi-user.target
+EOT
 
-systemctl start jenkins
-systemctl enable jenkins
----------------------------------------------------
-docker exec -it jenkins /bin/bash
-cat /var/jenkins_home/secrets/initialAdminPassword
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+#TO RETRIEVE PASSWORD
+#docker exec -it jenkins /bin/bash
+#cat /var/jenkins_home/secrets/initialAdminPassword
