1820ef8a7480a38dec6b1b5d381001aa2b09c043
azka-begh <97597065+azka-begh@users.noreply.github.com>
2024-01-28 16:28:40 +0530

    Update and rename Grafana-setup.txt to Grafana-setup-with-Docker.sh

--- Grafana-setup-with-Docker.sh
+++ Grafana-setup-with-Docker.sh
@@ -1,20 +1,17 @@
-docker run -d -p 3000:3000 --name=grafana grafana/grafana-enterprise
-
-
-
--------------------------------------------------------------------------
-vim /etc/systemd/system/grafana.service
--------------------------------------------------------------------------
+#!/bin/bash
+sudo cat <<EOT>> /etc/systemd/system/grafana.service
 [Unit]
 Description=grafana service
 After=network.target
-
 [Service]
-
 SyslogIdentifier=grafana.service
 ExecStartPre=-/usr/bin/docker create --name grafana -p 3000:3000 -v --restart=always grafana/grafana-enterprise
 ExecStart=/usr/bin/docker start -a grafana
 ExecStop=-/usr/bin/docker stop --time=0 grafana
-
 [Install]
 WantedBy=multi-user.target
+EOT
+
+sudo systemctl daemon-reload
+sudo systemctl start nexus
+sudo systemctl enable nexus 
