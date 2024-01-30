6c858494161637c6ead5841ac3a072fd2b92120a
azka-begh <97597065+azka-begh@users.noreply.github.com>
2024-01-30 18:55:31 +0530

    Update kubectl-and-kops-setup-ubuntu.sh

--- Ubuntu/kubectl-and-kops-setup-ubuntu.sh
+++ Ubuntu/kubectl-and-kops-setup-ubuntu.sh
@@ -26,3 +26,25 @@ kops update cluster --name k8skops.aab12.xyz --yes --state=s3://kops-s3-k8s-buck
 # Wait for 15 min atleast and run:
 kops validate cluster --state=s3://kops-s3-k8s-bucket
 # For retrying until 10min use --wait 10m option with validate
+
+# To stop kops cluster 
+export KOPS_STATE_STORE=s3://your-bucket-name
+kops get ig
+
+# edit your master and slave nodes
+kops edit ig <master-name>
+
+# Change maxsize and minsize to 0
+
+minsize: 0
+maxSize: 0
+
+# Update your cluster
+
+kops update cluster --yes
+# Rolling changes to stop the cluster
+
+kops rolling-update cluster --yes
+# Rolling changes to start the cluster
+
+kops rolling-update cluster --cloudonly --force --yes
